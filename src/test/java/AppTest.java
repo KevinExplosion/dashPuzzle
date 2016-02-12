f96fcd8944a42022d0e2306f87c44d5b50b241c1
kevin <mattisonii@yahoo.com>
2016-02-12 08:14:49 -0800

    Create puzzleInput.vtl & puzzleOutput.vtl

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,13 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.junit.*;
 import static org.junit.Assert.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 
 public class AppTest {
-  
+
 }
