b7483e9e091c201d9b4076896850767aab1cce44
kevin <mattisonii@yahoo.com>
2016-02-12 09:59:30 -0800

    Add passing junit specs for capital vowels and whole sentence.

--- src/main/java/DashPuzzle.java
+++ src/main/java/DashPuzzle.java
@@ -8,12 +8,38 @@ import java.util.ArrayList;
 
 public class DashPuzzle {
   public static void main(String[] args) {
-    // String layout = "templates/layout.vtl";
-    // staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+
+    get("/Puzzle", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/puzzleInput.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // get("/palresults", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   String phrase = request.queryParams("phraseInput");
+    //   model.put("sentence", sentence);
+    //   model.put("template", "templates/puzzleOutput.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
   }
 
     public static String dashPuzzle(String sentence) {
       sentence = sentence.replace('a', '-');
+      sentence = sentence.replace('e', '-');
+      sentence = sentence.replace('i', '-');
+      sentence = sentence.replace('o', '-');
+      sentence = sentence.replace('u', '-');
+      sentence = sentence.replace('y', '-');
+      sentence = sentence.replace('A', '-');
+      sentence = sentence.replace('E', '-');
+      sentence = sentence.replace('I', '-');
+      sentence = sentence.replace('O', '-');
+      sentence = sentence.replace('U', '-');
+      sentence = sentence.replace('Y', '-');
 
       return sentence;
     }
--- src/test/java/DashPuzzleTest.java
+++ src/test/java/DashPuzzleTest.java
@@ -10,8 +10,20 @@ import java.util.ArrayList;
 
 public class DashPuzzleTest {
   @Test
-  public void dashPuzzle_removesVowels_wordChanges() {
+  public void dashPuzzle_removesVowels_sentenceChanges() {
     DashPuzzle testDashPuzzle = new DashPuzzle();
     assertEquals("P-zzl-", testDashPuzzle.dashPuzzle("Puzzle"));
+  }
+
+  @Test
+  public void dashPuzzle_removesCapitalVowels_sentenceChanges() {
+    DashPuzzle testDashPuzzle = new DashPuzzle();
+    assertEquals("--lp", testDashPuzzle.dashPuzzle("Yelp"));
+  }
+
+  @Test
+  public void dashPuzzle_removesVowelsFromEntireSentence_sentenceChanges() {
+    DashPuzzle testDashPuzzle = new DashPuzzle();
+    assertEquals("Th- m-v-- t-tl- -s ---ng G-ns", testDashPuzzle.dashPuzzle("The movie title is Young Guns"));
   }
 }
